8f987477480991cc21ab61e9845ea77e281e412d
Franco <franco.mz92@hotmail.com>
2026-04-11 18:08:07 -0300

    Register the repositories

--- src/common/infrastructure/persistence/repositories/_registry.py
+++ src/common/infrastructure/persistence/repositories/_registry.py
@@ -1,7 +1,21 @@
 from src.common.application.ports.repository import IRepository
-from src.auth.infrastructure.persistance.repositories.users import UserRepository, IUserRepository
+from src.auth.infrastructure.persistance.repositories.users import IUserRepository, UserRepository
+from src.cattle.infrastructure.persistance.repositories.animals import IAnimalsRepository, AnimalRepository
+from src.cattle.infrastructure.persistance.repositories.animal_types import AnimalTypeRepository, IAnimalTypesRepository
+from src.finance.infrastructure.persistance.repositories.animal_suppiles import AnimalSuppliesRepository, IAnimalSuppliesRepository
+from src.finance.infrastructure.persistance.repositories.animal_supplie_types import SupplyTypesRepository, ISupplyTypesRepository
+from src.market.infrastructure.persistance.repositories.buyers import BuyersRepository, IBuyersRepository
+from src.market.infrastructure.persistance.repositories.sales import ISalesRepository, SalesRepository
+from src.finance.infrastructure.persistance.repositories.purchases import IPurchasesRepository, PurchasesRepository
 
 
 repositories_list: dict[type[IRepository], type[IRepository]] = {
     IUserRepository: UserRepository,
+    IAnimalsRepository: AnimalRepository,
+    IAnimalTypesRepository: AnimalTypeRepository,
+    IAnimalSuppliesRepository: AnimalSuppliesRepository,
+    ISupplyTypesRepository: SupplyTypesRepository,
+    IPurchasesRepository: PurchasesRepository,
+    IBuyersRepository: BuyersRepository,
+    ISalesRepository: SalesRepository,
 }
